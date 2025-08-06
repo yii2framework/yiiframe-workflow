60627a7811f54d2c529ffa71e1904c1c4c6d3fd2
hjp1011 <hjp1011@163.com>
2025-08-06 23:07:00 +0800

    init

---
+++ src/base/WorkflowInterface.php
@@ -0,0 +1,36 @@
+<?php
+namespace yiiframe\workflow\base;
+
+/**
+ * Interface implemented by Workflow objects.
+ */
+interface WorkflowInterface
+{
+	/**
+	 * Returns the id of this workflow
+	 * 
+	 * @return string the workflow id
+	 */
+	public function getId();
+	/**
+	 * Returns the id of the initial status for this workflow.
+	 *
+	 * @return string status id
+	 */
+	public function getInitialStatusId();
+	
+	/**
+	 * Returns the initial status instance for this workflow.
+	 * 
+	 * @return \yiiframe\workflow\base\StatusInterface the initial status instance
+	 * @throws \yiiframe\workflow\base\WorkflowException when no source component is available
+	 */
+	public function getInitialStatus();
+	/**
+	 * Returns an array containing all Status instances belonging to this workflow.
+	 * 
+	 * @return \yiiframe\workflow\base\StatusInterface[]  status list belonging to this workflow
+	 * @throws \yiiframe\workflow\base\WorkflowException when no source component is available
+	 */
+	public function getAllStatuses();	
+}
---
+++ src/source/file/IWorkflowDefinitionProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace yiiframe\workflow\source\file;
+
+/**
+ * This interface must be implemented by any PHP class that
+ * is able to provide a workflow definition. 
+ */
+interface IWorkflowDefinitionProvider
+{
+	/**
+	 * Returns the workflow definition in the form of an array.
+	 * @return array
+	 */
+	public function getDefinition();
+}
